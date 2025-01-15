027508267017d0ac7dc825f63f4331690367ac45
rok1sa <139523036+danny-codes@users.noreply.github.com>
2025-01-14 19:29:53 -0800

    test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) return 'wrong num';
+    let string = '';
+    for (let i = 0; i < times; i++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
