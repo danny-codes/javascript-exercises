95d951f21657da4987416135314fcad4bd77a24d
daniel <139523036+danny-codes@users.noreply.github.com>
2025-02-13 17:58:00 -0800

    exercise finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(sequence) {
+    let count;
+    if(typeof sequence !== 'number') {
+        count = parseInt(sequence);
+    } else {
+        count = sequence;
+    }
 
+    if (count < 0) return 'OOPS';
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = secondPrev;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
